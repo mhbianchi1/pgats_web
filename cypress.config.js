7b313c848d7bc454838aa45686adadaecf8b7039
unknown <mhbianchi1@gmail.com>
2024-08-11 21:12:01 -0300

    adicionando reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  watchForFileChanges: false,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
